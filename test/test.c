08cd89a2719cfdb326bcc23f8a6f3446716464b2
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-05-14 13:52:36 +0200

    fix: linked list

--- test/test.c
+++ test/test.c
@@ -10,22 +10,31 @@ int main(void)
 	ft_printf("LOG_LEVEL: %d\n", LOG_LEVEL);
 	ft_log("TEST LOG\n");
 	void* ptr = malloc(12 * sizeof(char));
-	if (!ptr)
+	if (!ptr) {
 		ft_putstr("Malloc failed\n");
+		return 0;
+	}
 	show_block_status(ptr);
 	show_alloc_mem();
 	ft_memcpy(ptr, "Hello World", 12);
 	ft_printf("ptr: %s\n", ptr);
 	void* ptr2 = realloc(ptr, 100 * sizeof(char));
-	if (!ptr2)
+	if (!ptr2) {
 		ft_putstr("Realloc failed\n");
+		return 0;
+	}
 	ft_printf("ptr2: %s\n", ptr2);
 	show_block_status(ptr2);
 	show_alloc_mem();
 	void* ptr3 = malloc(100 * sizeof(char));
-	if (!ptr3)
+	if (!ptr3) {
 		ft_putstr("Malloc failed\n");
+		return 0;
+	}
+	ft_putstr("ptr3 allocated\n");
 	show_alloc_mem();
 	free(ptr2);
+	free(ptr3);
+	show_alloc_mem();
 	return 0;
 }
