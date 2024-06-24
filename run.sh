7d0d94fda93a9f8c289363ef64e819a5e238e1f2
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-06-24 14:14:19 +0200

    feat: replace shell process with program process

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 HOSTTYPE=$(uname -m)_$(uname -s)
-M_LOGLEVEL="DEBUG" LD_PRELOAD="libft_malloc_$HOSTTYPE.so" LD_LIBRARY_PATH=$(pwd) $@
+M_LOGLEVEL="INFO" LD_PRELOAD="libft_malloc_$HOSTTYPE.so" LD_LIBRARY_PATH=$(pwd) exec $@
